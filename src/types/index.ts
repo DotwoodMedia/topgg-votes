b3d5dde9a62f0eaa7487c2b86020bdfb76187dde
Dotwood Media <info@dotwood.media>
2023-08-06 15:29:31 +0200

    Export all types

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+// Client
+export type { VoteClientConfigOptions } from './Client/VoteClientConfigOptions';
+export type { VoteClientEvents } from './Client/VoteClientEvents';
+
+// Events
+export type { BotVoteEvent } from './Events/BotVoteEvent';
+export type { ServerVoteEvent } from './Events/ServerVoteEvent';
+
+export { VoteWebhookManagerOptions } from './VoteWebhookManagerOptions';
