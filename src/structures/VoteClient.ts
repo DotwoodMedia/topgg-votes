994d80122c9a1c5dc0a1feccbda660851e3ac413
Dotwood Media <info@dotwood.media>
2023-08-14 17:28:39 +0200

    Moved auth token functions to every function

--- src/structures/VoteClient.ts
+++ src/structures/VoteClient.ts
@@ -12,10 +12,6 @@ export class VoteClient extends EventEmitter {
     this._authToken = options?.token ?? '';
     this._webhookOptions = options?.webhook;
 
-    if (!this._authToken || this._authToken === '') {
-      throw new Error('[Top.gg Votes] Missing token!');
-    }
-
     if (this._webhookOptions) {
       new VoteWebhookManager({
         ...this._webhookOptions,
@@ -53,21 +49,37 @@ export class VoteClient extends EventEmitter {
     return this;
   }
 
+  public hasToken(): boolean {
+    return !!this._authToken || this._authToken !== '';
+  }
+
   public async getVotes(): Promise<ShortUser[]> {
+    if (!this.hasToken()) {
+      throw new Error('[Top.gg Votes] Missing token!');
+    }
     return await new Api(this._authToken).getVotes();
   }
 
   public async hasVoted(userId: string): Promise<boolean> {
+    if (!this.hasToken()) {
+      throw new Error('[Top.gg Votes] Missing token!');
+    }
     if (!userId) throw new Error('[Top.gg Votes] Missing user ID!');
     return await new Api(this._authToken).hasVoted(userId);
   }
 
   public async getBot(botId: string): Promise<BotInfo> {
+    if (!this.hasToken()) {
+      throw new Error('[Top.gg Votes] Missing token!');
+    }
     if (!botId) throw new Error('[Top.gg Votes] Missing bot ID!');
     return await new Api(this._authToken).getBot(botId);
   }
 
   public async getUser(userId: string): Promise<UserInfo> {
+    if (!this.hasToken()) {
+      throw new Error('[Top.gg Votes] Missing token!');
+    }
     if (!userId) throw new Error('[Top.gg Votes] Missing user ID!');
     return await new Api(this._authToken).getUser(userId);
   }
